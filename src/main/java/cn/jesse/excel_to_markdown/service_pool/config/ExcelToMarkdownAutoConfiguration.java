711c13884c6dc13d21fcab0ee23a65c3bba941c5
浩进 朱 <3191955858@qq.com>
2025-09-17 15:41:05 +0800

    Excel -> Markdown 转换服务 Spring 自动配置类。

---
+++ src/main/java/cn/jesse/excel_to_markdown/service_pool/config/ExcelToMarkdownAutoConfiguration.java
@@ -0,0 +1,41 @@
+package cn.jesse.excel_to_markdown.service_pool.config;
+
+import cn.jesse.excel_to_markdown.service_pool.ConvertServicePoolManager;
+import cn.jesse.excel_to_markdown.service_pool.impl.DefaultConvertServicePoolManager;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/** Excel -> Markdown 转换服务 Spring 自动配置类。*/
+@Configuration
+@ConditionalOnProperty(
+    prefix         = "app.excel-to-markdown",
+    name           = "enabled",
+    havingValue    = "true",
+    matchIfMissing = false
+)
+@EnableConfigurationProperties(ExcelToMarkdownProperties.class)
+public class ExcelToMarkdownAutoConfiguration
+{
+    public ExcelToMarkdownAutoConfiguration()
+    {
+        System.out.println(
+            "Construct instants of class ExcelToMarkdownAutoConfiguration..."
+        );
+    }
+
+    /**
+     * 按照 {@link ExcelToMarkdownProperties} 提供的配置，自动创建转换服务。
+     */
+    @Bean
+    @ConditionalOnMissingBean(ConvertServicePoolManager.class)
+    public ConvertServicePoolManager
+    convertServicePoolManager(@NotNull ExcelToMarkdownProperties properties)
+    {
+        return new
+        DefaultConvertServicePoolManager(properties.getProcessCount());
+    }
+}
