30d2758f3ec2225dd2e6ecd7839dfe3105c380f3
浩进 朱 <3191955858@qq.com>
2025-09-17 16:34:12 +0800

    文件扩展名检查器。

---
+++ src/main/java/cn/jesse/excel_to_markdown/service_pool/utils/FileExtensionChecker.java
@@ -0,0 +1,56 @@
+package cn.jesse.excel_to_markdown.service_pool.utils;
+
+import cn.jesse.excel_to_markdown.service_pool.exception.NotSupportFileExtension;
+
+import java.util.Locale;
+import java.util.Set;
+
+/** 文件扩展名检查器。*/
+public class FileExtensionChecker
+{
+    /** 支持进行转换的文件扩展名集合。*/
+    private static final Set<String>
+    SUPPORT_FILE_EXTENSION = Set.of(".xlsx", ".xlsm", ".xlsb", ".xls", ".csv");
+
+    /** 检查某个字符串是否为空串。*/
+    private static boolean
+    isNotEmptyString(String string) {
+        return (string != null) && !(string.trim().isEmpty());
+    }
+
+    /** 执行检查 */
+    public static void
+    check(String fileName)
+    {
+        if(!isNotEmptyString(fileName))
+        {
+            throw new
+            NotSupportFileExtension("File name is null or empty!");
+        }
+
+        int dotIndex = fileName.lastIndexOf('.');
+
+        if (dotIndex == -1)
+        {
+            throw new
+            NotSupportFileExtension(
+                String.format("File %s don't have extension!", fileName)
+            );
+        }
+
+        final String extension
+            = fileName.substring(dotIndex)
+                      .toLowerCase(Locale.ROOT);
+
+        if (!SUPPORT_FILE_EXTENSION.contains(extension))
+        {
+            throw new
+            NotSupportFileExtension(
+                String.format(
+                    "File extension: %s not support! Only support type of %s extensions!",
+                    extension, SUPPORT_FILE_EXTENSION
+                )
+            );
+        }
+    }
+}
