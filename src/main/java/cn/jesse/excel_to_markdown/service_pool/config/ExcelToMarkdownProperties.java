b2e16c5d2fdfc660db47e37f300d26cda0b1dd4d
浩进 朱 <3191955858@qq.com>
2025-09-17 16:31:40 +0800

    依赖 Spring 自动配置属性类。

---
+++ src/main/java/cn/jesse/excel_to_markdown/service_pool/config/ExcelToMarkdownProperties.java
@@ -0,0 +1,31 @@
+package cn.jesse.excel_to_markdown.service_pool.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * 依赖 Spring 自动配置属性类。
+ *
+ * <p>用法：</p>
+ * <ol>
+ *     <li>
+ *         在配置文件中使用
+ *              app.excel-to-markdown.enabled=true
+ *         来启用本转换服务
+ *     </li>
+ *     <li>
+ *         使用 app.excel-to-markdown.process-count=?
+ *         来控制服务进程的数量
+ *     </li>
+ * </ol>
+ */
+@Data
+@ConfigurationProperties(prefix = "app.excel-to-markdown")
+public class ExcelToMarkdownProperties
+{
+    /** 是否启动本服务？（默认不启用）*/
+    private boolean enabled = false;
+
+    /** 服务最大进程数是？（默认为 4）*/
+    private int processCount = 4;
+}
