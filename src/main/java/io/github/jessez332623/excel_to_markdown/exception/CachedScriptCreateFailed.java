0734cc79042cdc0eb3d9811a0949106992f0cc8b
浩进 朱 <3191955858@qq.com>
2025-09-18 15:23:10 +0800

    创建缓存脚本失败时最终会 re-throw 成本异常。

--- src/main/java/io/github/jessez332623/excel_to_markdown/exception/CachedScriptCreateFailed.java
+++ src/main/java/io/github/jessez332623/excel_to_markdown/exception/CachedScriptCreateFailed.java
@@ -1,4 +1,4 @@
-package cn.jesse.excel_to_markdown.service_pool.exception;
+package io.github.jessez332623.excel_to_markdown.exception;
 
 /** 创建缓存脚本失败时最终会 re-throw 成本异常。*/
 public class CachedScriptCreateFailed
