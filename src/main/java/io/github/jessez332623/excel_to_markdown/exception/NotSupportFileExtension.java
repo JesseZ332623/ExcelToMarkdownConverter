bdeb36c7c055d6dd38b59b93a0b5f166d90cfabd
浩进 朱 <3191955858@qq.com>
2025-09-18 15:24:30 +0800

    检查到不支持的文件扩展名时抛出本异常。

--- src/main/java/io/github/jessez332623/excel_to_markdown/exception/NotSupportFileExtension.java
+++ src/main/java/io/github/jessez332623/excel_to_markdown/exception/NotSupportFileExtension.java
@@ -1,4 +1,4 @@
-package cn.jesse.excel_to_markdown.service_pool.exception;
+package io.github.jessez332623.excel_to_markdown.exception;
 
 /** 检查到不支持的文件扩展名时抛出本异常。*/
 public class NotSupportFileExtension
