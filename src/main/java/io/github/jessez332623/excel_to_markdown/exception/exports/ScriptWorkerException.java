c96464f96a81c463482d75b9aceac796977df9c6
浩进 朱 <3191955858@qq.com>
2025-09-29 11:30:17 +0800

    在转换过程中出现的任何异常，最终都会 re-throw 成本异常

--- src/main/java/io/github/jessez332623/excel_to_markdown/exception/exports/ScriptWorkerException.java
+++ src/main/java/io/github/jessez332623/excel_to_markdown/exception/exports/ScriptWorkerException.java
@@ -1,4 +1,4 @@
-package io.github.jessez332623.excel_to_markdown.exception;
+package io.github.jessez332623.excel_to_markdown.exception.exports;
 
 /** 在转换过程中出现的任何异常，最终都会 re-throw 成本异常。*/
 public class ScriptWorkerException extends RuntimeException
