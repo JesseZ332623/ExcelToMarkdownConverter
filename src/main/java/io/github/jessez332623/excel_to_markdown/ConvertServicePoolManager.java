0b6ae8772d43b06e54715d26b85177f9b75b9ec1
浩进 朱 <3191955858@qq.com>
2025-09-29 11:29:47 +0800

    Excel 表格转 Markdown Python 服务池管理器接口

--- src/main/java/io/github/jessez332623/excel_to_markdown/ConvertServicePoolManager.java
+++ src/main/java/io/github/jessez332623/excel_to_markdown/ConvertServicePoolManager.java
@@ -1,6 +1,8 @@
 package io.github.jessez332623.excel_to_markdown;
 
-import io.github.jessez332623.excel_to_markdown.exception.ScriptWorkerException;
+import io.github.jessez332623.excel_to_markdown.exception.exports.ScriptWorkerException;
+
+import java.nio.file.Path;
 
 /** Excel 表格转 Markdown Python 服务池管理器接口。*/
 public interface ConvertServicePoolManager
@@ -8,12 +10,17 @@ public interface ConvertServicePoolManager
     /**
      * 开放的执行转换接口 Excel -> Markdown
      *
-     * @param tablePath 表格临时文件路径
+     * @param tablePath 表格文件路径
      *
      * @return 转换完成后的 Markdown 文本
      *
      * @throws ScriptWorkerException 服务启动失败，转换失败最终抛出本异常
      */
-    String
-    convertTableToMarkdown(String tablePath);
+    default String
+    convertTableToMarkdown(Path tablePath) throws ScriptWorkerException
+    {
+        // 给一个安全的默认实现，
+        // 避免使用者禁用依赖的时候出现注入错误。
+        return null;
+    }
 }
